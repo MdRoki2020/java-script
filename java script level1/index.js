401ca52554fd7bd2ccd08292e5a4c6f7063dae25
Md.Roki <73433502+MdRoki2020@users.noreply.github.com>
2021-06-05 01:46:27 +0600

    Initial commit

---
+++ java script level1/index.js
@@ -0,0 +1,4 @@
+    alert("please go to Rs ROKI facebook ID");
+    document.write("<h1>I Love Bangladesh</h1>");
+    document.write("phone: 01717453205<br/>");
+    document.write("study at Daffodil International university<br/>");
