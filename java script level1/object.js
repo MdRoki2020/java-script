338e05b222cd8c01eac18b73bbe4e8a146d7f04f
Md. Rakibul Islam Roki <rakibul15-13996@diu.edu.bd>
2023-12-23 23:53:32 +0600

    Add files via upload

---
+++ java script level1/object.js
@@ -0,0 +1,13 @@
+var car={
+	
+	name: "Ferary",
+	prize:15000000,
+	color:"red wine",
+	size:14,
+	
+}
+
+document.querySelector("#name").innerHTML=car.name;
+document.querySelector("#prize").innerHTML=car.prize;
+document.querySelector("#color").innerHTML=car.color;
+document.querySelector("#size").innerHTML=car.size;
